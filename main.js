c566e8eaadf06196e06579c95e0d9ee3f47e8f5c
Acquite <acquitefx@gmail.com>
2023-07-13 23:31:46 +0100

    For now, just render it instead of the bookwork

--- main.js
+++ main.js
@@ -19,9 +19,16 @@
         dynamicSubmitButton = document.getElementById("skill-delivery-submit-button");
     });
 
-    patcher.after("render", findReact(document.getElementsByClassName('wac-overlay')[0]).__proto__, function() {
+    patcher.after("render", findReact(document.getElementsByClassName('wac-overlay')[0]).__proto__, function(_, res) {
         const answers = localStorageHandler.get(this.props.bookworkCode);
-        katex.render(answers, document.getElementsByClassName("wac-text")[0], { throwOnError: false });
+        const container = findInReactTree(res, r => r.props.children[1].props.className?.includes("bookwork-code"));
+
+        if (!container) return;
+
+        container.props.children[1].key = "answer-container";
+        container.props.children[1].props.children = ["Answers: ", answers.join("")]
+
+        return res;
     })
 
     dynamicSubmitButton?.addEventListener("click", storeAnswers);
--- patch.js
+++ patch.js
@@ -1,6 +1,7 @@
-const loadScript = (name) => {
+const loadScript = (src, async = false) => {
     const script = document.createElement('script');
-    script.src = chrome.runtime.getURL(name);
+    script.src = src;
+    script.async = async;
     script.onload = function() { this.remove(); };
 
     (document.head || document.documentElement).appendChild(script);
@@ -8,4 +9,4 @@ const loadScript = (name) => {
 
 fetch("https://raw.githubusercontent.com/acquitelol/CutestBypass/main/manifest.json")
     .then(r => r.json())
-    .then(r => r.web_accessible_resources[0].resources.map(loadScript));
+    .then(r => r.web_accessible_resources[0].resources.map(item => loadScript(chrome.runtime.getURL(item))));
