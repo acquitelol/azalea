c517c2d92645c794755d0c6414fb1779c98ea54a
Acquite <acquitefx@gmail.com>
2023-07-16 00:47:37 +0100

    Get image assets from /extension instead of /dist

--- src/api/core/utilities/common.ts
+++ src/api/core/utilities/common.ts
@@ -1,6 +1,6 @@
 const cuteName = "Rosie :3";
 const wait = (time: number) => new Promise(res => setTimeout(res, time));
-const getImage = (name: string) => `https://raw.githubusercontent.com/acquitelol/CutestBypass/main/dist/assets/${name}`;
+const getImage = (name: string) => `https://raw.githubusercontent.com/acquitelol/CutestBypass/main/extension/assets/${name}`;
 
 export default {
     cuteName,
