4fbb7a190865caad473d743c21593555615cf2c7
Acquite <acquitefx@gmail.com>
2023-07-22 21:39:36 +0100

    Minor improvements towards Links & Boowork Toggle
    Stuff like this always happens as soon as you push some big update :c

--- src/core/patches/menu/settings.tsx
+++ src/core/patches/menu/settings.tsx
@@ -72,8 +72,11 @@ const ToggleBookwork = () => {
             marginBottom: "0"
         }}
     >
+        <h3 style={{ marginRight: "0.5em" }}>
+            Autobookwork
+        </h3>
         <Button 
-            text={`${enabled ? "Disable" : "Enable"} autobookwork`}
+            text={enabled ? "Disable" : "Enable"}
             className={"special-btn"}
             onClick={() => setEnabled(previous => !previous)}
         />
